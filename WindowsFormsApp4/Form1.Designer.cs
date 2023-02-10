c41b9bbe8e179081e3ee313d6cf3a1cff10d99f8
ASUS <ASUS@DESKTOP-O86GR9L>
2023-02-11 01:51:37 +0400

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.londonbtn = new System.Windows.Forms.Button();
+            this.bakubtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // londonbtn
+            // 
+            this.londonbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.londonbtn.Location = new System.Drawing.Point(62, 246);
+            this.londonbtn.Name = "londonbtn";
+            this.londonbtn.Size = new System.Drawing.Size(283, 55);
+            this.londonbtn.TabIndex = 2;
+            this.londonbtn.Text = "LONDON";
+            this.londonbtn.UseVisualStyleBackColor = true;
+            this.londonbtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.londonbtn_MouseClick);
+            // 
+            // bakubtn
+            // 
+            this.bakubtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bakubtn.Location = new System.Drawing.Point(449, 246);
+            this.bakubtn.Name = "bakubtn";
+            this.bakubtn.Size = new System.Drawing.Size(196, 55);
+            this.bakubtn.TabIndex = 3;
+            this.bakubtn.Text = "BAKU";
+            this.bakubtn.UseVisualStyleBackColor = true;
+            this.bakubtn.MouseClick += new System.Windows.Forms.MouseEventHandler(this.bakubtn_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cyan;
+            this.BackgroundImage = global::WindowsFormsApp4.Properties.Resources.Bakiu;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bakubtn);
+            this.Controls.Add(this.londonbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button londonbtn;
+        private System.Windows.Forms.Button bakubtn;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp4.Properties;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+
+        Label bakuLbl=new Label();
+        Label bakuDate=new Label();  
+        Label londonLbl=new Label();     
+        Timer mytimer=new Timer();
+        Timer londonTimer=new Timer();  
+        Timer bakuTimer=new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            mytimer.Interval=1000;
+            mytimer.Tick+=Mytimer_Tick;
+            mytimer.Start();
+        }
+
+        private void Mytimer_Tick(object sender, EventArgs e)
+        {
+
+            //throw new NotImplementedException();
+        }
+
+        private void londonbtn_MouseClick(object sender, MouseEventArgs e)
+        {
+            mytimer.Stop();
+            bakuTimer.Stop();
+            foreach (var control in Controls)
+            {
+                if (control is Label)
+                {
+                    (control as Label).Visible=false;
+                }
+            }
+            this.BackgroundImage=Resources.LLondon;
+            londonLbl.Text="London ";
+            londonLbl.ForeColor=Color.Black;
+            londonLbl.ForeColor=Color.Black;
+            londonLbl.Location=new Point(15,560);
+            londonLbl.AutoSize=true;
+            londonLbl.Font=new Font("Bodoni MT", 39, FontStyle.Bold);
+            londonLbl.Visible=true;
+            this.Controls.Add(londonLbl);   
+            londonTimer.Interval=1000;
+            londonTimer.Tick+=LondonTimer_Tick;
+            londonTimer.Start();
+        }
+
+        private void LondonTimer_Tick(object sender, EventArgs e)
+        {
+            londonLbl.Text=$"Time : {DateTime.Now.AddHours(-4):HH:mm:ss}";
+
+        }
+
+        private void bakubtn_MouseClick(object sender, MouseEventArgs e)
+        {
+            mytimer.Stop();
+            londonTimer.Stop();
+            foreach (var control in Controls)
+            {
+                if (control is Label)
+                {
+                    (control as Label).Visible=false;
+                }
+            }
+            this.BackgroundImage=Resources.Bakiu;
+            bakuLbl.Text="Baku ";
+            bakuLbl.ForeColor=Color.Black;
+            bakuLbl.ForeColor=Color.Black;
+            bakuLbl.Location=new Point(15, 560);
+            bakuLbl.AutoSize=true;
+            bakuLbl.Font=new Font("Bodoni MT", 39, FontStyle.Bold);
+            bakuLbl.Visible=true;
+            bakuDate.Visible=true;
+            this.Controls.Add(bakuLbl);
+            bakuTimer.Interval=1000;
+            bakuTimer.Tick+=BakuTimer_Tick;
+            bakuTimer.Start();
+
+        }
+
+        private void BakuTimer_Tick(object sender, EventArgs e)
+        {
+            bakuLbl.Text=$"Time : {DateTime.Now:HH:mm:ss}";
+        }
+        
+
+    }
+}
